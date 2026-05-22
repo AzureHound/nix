ac8627323dd7d4d927f9595afa37095315bf7cd6
Eden <azurehound.landline090@slmail.me>
2026-05-22 23:55:53 +0530

    ⚙️ config: nixpkgs url changed

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Init flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.xz";
 
     disko = {
       url = "github:nix-community/disko";
